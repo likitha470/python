fe46e638ee83657603d3854247d3cca13736f62b
likitha470 <likitharaib.s@gmail.com>
2024-07-22 14:50:00 +0530

    second commit

---
+++ variables/sum.py
@@ -0,0 +1,7 @@
+a=float(input("enter the first number:"))
+b=float(input("enter the second number:"))
+c=float(input("enter the third number:"))
+d=float(input("enter the fourth number:"))
+e=float(input("enter the fifth number:"))
+f=float(input("enter the sixth number:"))
+print("sum:",a+b+c+d+e+f)
