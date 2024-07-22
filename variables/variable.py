337a53b04e70c7f4037dd9d4c638d7c345fd64ef
likitha470 <likitharaib.s@gmail.com>
2024-07-22 10:16:01 +0530

    third commit

--- variables/variable.py
+++ variables/variable.py
@@ -1,1 +1,16 @@
-print('likitha')
+print("hello world")
+print("likitha rai")
+name="likitha"
+age=18
+price=13.55
+print("my name is:",name)
+print("my age is:",age)
+print("price:",price)
+print(type(age))
+print(type(price))
+print(type(name))
+age=12
+b=None
+old=False
+print(type(old))
+print(type(b))
