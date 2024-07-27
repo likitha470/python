afcc3d5f5621cf3410feffdff5cec256234ff6af
likitha470 <likitharaib.s@gmail.com>
2024-07-27 12:59:23 +0530

    Added new files

---
+++ for_loop/pass.py
@@ -0,0 +1,14 @@
+for i in range(1,10) :#pass
+    pass
+
+print("work")
+
+
+i=1
+if(i<5):
+    pass
+
+
+
+print("passes")
+print("run")
---
+++ for_loop/pro3.py
@@ -0,0 +1,18 @@
+i=1
+n=10
+sum=0
+while i<=n:
+    sum+=i
+    i+=1
+
+
+print("sum:",sum)
+
+n=10
+sum=0
+for i in range(1,n+1):
+    sum+=i
+
+print("sum:",sum)     
+
+
